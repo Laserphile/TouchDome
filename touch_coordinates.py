ee1fdbebaa85f0c21fdc44008a2e818c722b0706
Emcee <mattjcallaghan@gmail.com>
2017-09-16 06:26:11 +0800

    Improved touch coordinates

--- touch_coordinates.py
+++ touch_coordinates.py
@@ -31,17 +31,24 @@ while(True):
     frame = frame.astype(np.uint8)
     ret, frame = cv2.threshold(frame,127,255,cv2.THRESH_BINARY_INV)
     frame = cv2.bitwise_and(frame, frame, mask = mask)
-    #frame = cv2.resize(frame, (200,200), fx=1, fy=1)
+    frame = cv2.resize(frame, (1920/2,1080/2), fx=1, fy=1)
+
 
     matrix = np.asarray(frame)
     matrix_coords = zip(*np.where(matrix == 255))
-    #print matrix
+
+
+    img = np.zeros((1080/2,1920/2,3), np.uint8)
+    old_img = img
+
     if matrix_coords:
             x = matrix_coords[(0)]
+            cv2.circle(img,(x[1],x[0]),20,(255,255,255),-1)
             print x
 
+
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame',img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
