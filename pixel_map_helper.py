24c37f0a61ff40495bfd46a92bf5d05f7ca42b7a
Emcee <mattjcallaghan@gmail.com>
2017-09-19 20:22:08 +0800

    Change to Python3

--- pixel_map_helper.py
+++ pixel_map_helper.py
@@ -13,39 +13,37 @@
 import numpy as np
 import cv2
 
-global mouseX,mouseY,b
-mouseX,mouseY = 0,0
+mouseX = 0
+mouseY = 0
 
-b = np.array([0,0,0], np.uint8)
+b = np.array([0, 0, 0], np.uint8)
 l = 1
 
-def draw_circle(event,x,y,flags,param):
-    global mouseX,mouseY,b,l
-    mouseX,mouseY = x,y
+
+def draw_circle(event, x, y):
+    global mouseX, mouseY, b, l
+    mouseX, mouseY = x, y
     if event == cv2.EVENT_LBUTTONDOWN:
-        cv2.circle(img,(x,y),5,(255,0,0),-1)
-        a = np.array([x,y,l])
-        b = np.vstack((a,b))
+        cv2.circle(img, (x, y), 5, (255, 0, 0), -1)
+        a = np.array([x, y, l])
+        b = np.vstack((a, b))
         l = b[0]
         l = l[2] + 1
-        print b
-        mouseX,mouseY = x,y
+        print(b)
+        mouseX, mouseY = x, y
 
-img = cv2.imread('h2o_sign.png',1)
+
+img = cv2.imread('h2o_sign.png', 1)
 
 cv2.namedWindow('image')
-cv2.setMouseCallback('image',draw_circle)
+cv2.setMouseCallback('image', draw_circle)
 
-
-
-while(1):
-    cv2.imshow('image',img)
+while 1:
+    cv2.imshow('image', img)
     k = cv2.waitKey(20) & 0xFF
     if k == 27:
-        print "Enter a file name:",
+        print("Enter a file name:")
         filename = raw_input()
         np.savetxt(filename, b)
         np.save(filename, b)
         break
-    elif k == ord('a'):
-        print array
