e5821dc91b319c8edffa0c4d8e70ddd9dc02c32d
derwentx <derwentx@gmail.com>
2017-10-20 17:37:00 +1100

    💄 Pythonified things

--- serial_stream_test.py
+++ serial_stream_test.py
@@ -7,38 +7,37 @@
 import serial
 import numpy as np
 
-ser = serial.Serial('COM6', 9600)  # open serial port
-print(">", ser.name)         # check which port was really used
-serial_in = ser.readline() # wait for micro to say something
-print(serial_in)
+MOCK_SERIAL = False
 
-pixel_array = np.arange(15).reshape(3, 5)
+if not MOCK_SERIAL:
+    ser = serial.Serial('COM6', 9600)  # open serial port
+    print(">", ser.name)         # check which port was really used
+    serial_in = ser.readline() # wait for micro to say something
+    print(serial_in)
 
-pixel_array = ([
-    [1,  2,  3],
-    [4,  5,  6],
-    [7,  8,  9],
-    [10,  11,  12],
-    [13,  14,  15]])
+pixel_array = np.arange(15).reshape(5, 3)
+
+# This is the same as
+# pixel_array = np.array([
+#     [1,  2,  3],
+#     [4,  5,  6],
+#     [7,  8,  9],
+#     [10,  11,  12],
+#     [13,  14,  15]])
 
 server_done = 0
 
+if MOCK_SERIAL:
+    class Ser(object):
+        """ Used to mock the serial output for debugging """
+        def write(self, bytestr):
+            print("Serial writing bytestr: %s" % bytestr)
+    ser = Ser()
+
 def SerialTx():
-    bytes_sent = 0
-    num_pixels = 5
-    byte = 0
-    print(">Server transmitting")
-    while (bytes_sent < num_pixels):
-        col = 0
-        while (col < 3):
-            byte = pixel_array[bytes_sent][col]
-            string_byte = str(byte) + " \n"
-            ser.write(string_byte.encode('ascii'))
-            print(">", string_byte) # use this to echo what is sent to the serial port
-            col = col + 1
-        bytes_sent = bytes_sent + 1
-    print(">Server done")
-    server_done = 1
+    for byte in pixel_array.flatten():
+        string_byte = "%s \n" % byte
+        ser.write(string_byte.encode('ascii'))
 
 def SerialRx():
     serial_in = ser.readline()
