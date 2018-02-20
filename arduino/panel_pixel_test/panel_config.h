38034fd21322d9a76ed1fe2a81fce1e37eab1c3f
derwentx <derwentx@gmail.com>
2018-02-20 20:13:55 +1100

    more realistic lengths on actual live, not staging

--- arduino/panel_pixel_test/panel_config.h
+++ arduino/panel_pixel_test/panel_config.h
@@ -3,31 +3,31 @@
     #define NEEDS_CLK 0
     #define PANEL_00_DATA_PIN 6
     #define PANEL_00_CLK_PIN 0
-    #define PANEL_00_LEN 333
+    #define PANEL_00_LEN 100
     #define PANEL_01_DATA_PIN 7
     #define PANEL_01_CLK_PIN 0
-    #define PANEL_01_LEN 303
+    #define PANEL_01_LEN 90
     #define PANEL_02_DATA_PIN 0
     #define PANEL_02_CLK_PIN 0
-    #define PANEL_02_LEN 333
+    #define PANEL_02_LEN 100
     #define PANEL_03_DATA_PIN 0
     #define PANEL_03_CLK_PIN 0
-    #define PANEL_03_LEN 333
+    #define PANEL_03_LEN 100
     #define MAX_PANELS 4
 #else // Matt's Live Setup on Teensy 3.2
     #define PANEL_TYPE APA102
     #define NEEDS_CLK 1
     #define PANEL_00_DATA_PIN 7
     #define PANEL_00_CLK_PIN 13
-    #define PANEL_00_LEN 9
+    #define PANEL_00_LEN 333
     #define PANEL_01_DATA_PIN 7
     #define PANEL_01_CLK_PIN 14
-    #define PANEL_01_LEN 12
+    #define PANEL_01_LEN 303
     #define PANEL_02_DATA_PIN 11
     #define PANEL_02_CLK_PIN 13
-    #define PANEL_02_LEN 9
+    #define PANEL_02_LEN 333
     #define PANEL_03_DATA_PIN 11
     #define PANEL_03_CLK_PIN 14
-    #define PANEL_03_LEN 9
+    #define PANEL_03_LEN 333
     #define MAX_PANELS 4
 #endif
